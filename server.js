e3c05175c3baf5dcaa3d5d8af0e632fea432648b
Thai Tin Khang <kinyiasgit@gmail.com>
2024-02-14 19:40:53 +0700

    Update corsOptions allow origin

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000
 server.use(express.json())
 // server.use(cors())
 const corsOptions = {
-    origin: 'http://127.0.0.1:5500',
+    origin: ['http://127.0.0.1:5500', 'http://localhost:3000', 'yourwebsite'],
     credentials: true, // Allow credentials
 }
 
